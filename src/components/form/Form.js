26fc54e072d8b05c6d411af7720444f69f971ad1
AhmedAhmedHassan <ahmed.hassan@aru.edu.eg>
2021-05-06 01:01:55 +0200

    submit the form

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,33 +1,84 @@
+import React , {useState} from 'react';
 import './form.css';
 
 const Form=()=>{
+
+  // another way of state 
+// const [enteredInputs, setEnteredInputs] = useState({
+//   enteredTitle : '',
+//   enteredAge : '',
+//   enteredDate : ''
+// })
+
+const [enteredTitle, setEnteredTitle] = useState('');
+const [enteredAge, setEnteredAge] = useState('');
+const [enteredDate, setEnteredDate] = useState('');
+
+const titleHandler = event =>{
+  setEnteredTitle(event.target.value);
+//    setEnteredInputs((previousState)=>{
+//      return{
+//       ...previousState,
+//       enteredTitle : event.target.value
+//      }
+      
+//   })
+ }
+
+const ageHandler = event =>{
+  setEnteredAge(event.target.value);
+  
+}
+
+const dateHandler = event =>{
+  setEnteredDate(event.target.value);
+  
+}
+
+const submitHandler = event =>{
+  event.preventDefault();
+
+  const output = {
+    title : enteredTitle,
+    age : enteredAge,
+    date : new Date(enteredDate)
+  }
+
+console.log(output)
+setEnteredTitle('')
+setEnteredAge('')
+setEnteredDate('')
+
+}
+
     return(
-          <div>
-              <div class="container">
-  <form action="" >
+
+<div>
+<div class="container">
+  <form onSubmit={submitHandler}>
   <div class="row">
     <div class="col-25">
       <label for="fname"> Name</label>
     </div>
     <div class="col-75">
-      <input type="text" id="fname" name="firstname" placeholder="Your name.." />
+      <input type="text" id="fname" name="firstname" placeholder="Your name.." value={enteredTitle}  onChange={titleHandler} />
     </div>
   </div>
   
   <div class="row">
     <div class="col-25">
-      <label for="country">Country</label>
+      <label for="country">Age</label>
     </div>
     <div class="col-75">
-      <input type="number"  name="age"  />
+      <input type="number"  name="age" onChange={ageHandler} value={enteredAge} />
     </div>
   </div>
   <div class="row">
     <div class="col-25">
-      <label for="subject">Subject</label>
+      <label for="subject">Date</label>
     </div>
     <div class="col-75">
-    <input type="date"  name="date"  />
+    <input type="date"  name="date" value={enteredDate} onChange={dateHandler} />
     </div>
   </div>
   <div class="row">
