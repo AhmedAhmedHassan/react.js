1d83ac35637c512eb2a1b1f138f345c1339f9dc6
AhmedAhmedHassan <ahmed.hassan@aru.edu.eg>
2021-05-31 04:14:54 -0700

    added data component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import React from 'react';
 import AddUser from './Components/Form/Form'
+import Data from './Components/Data/Data'
 function App() {
   return (
+    <div>
      <AddUser/>
+     <Data/>
+    </div> 
   );
 }
 
---
+++ src/Components/Data/Data.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import  './Data.css';
+
+const Data = (props) =>{
+    return (
+            <div class="multi-button">
+            <p><button className="data">This is the first element</button></p>
+            <p><button className="data">This is the first element</button></p>
+           
+            </div>
+    );
+}
+export default Data
